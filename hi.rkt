6f84d110b5ee6f46d0aa782bc8c28eeb6009953c
JPablo <juapabgonzalez@utp.edu.co>
2023-08-19 23:41:31 -0500

    functions in racket

--- hi.rkt
+++ hi.rkt
@@ -1,1 +1,15 @@
+#lang racket
+
+(define (factorial n)
+  (if (= n 0)
+      1
+      (* n (factorial (- n 1)))))
+
+(display "Ingrese un número para calcular su factorial: ")
+(define num (read))
+(display "El factorial de ")
+(display num)
+(display " es ")
+(display (factorial num))
+(newline)
 
